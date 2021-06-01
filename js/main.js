3e39cee6766bed477aff541cb65bfb0d7b58f4d9
HelikopterUA <helikopterua@gmail.com>
2021-06-01 18:45:46 +0300

    01.06.21

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-$(function(){
+$(function () {
+
+    $('.about__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/slider/next.svg"></button>'
+    });
+
+    $('.route__slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/slider/prev.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/slider/next.svg"></button>'
+    });
 
 });
